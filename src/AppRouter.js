9ec85be456530c49d6e6cc1876f52ed3b8df26a5
Anthony Nixon <anixon604@gmail.com>
2019-03-08 17:41:00 +0100

    created basic login form: css styles piggyback off contact id of index.css, added to router but no actions or reducers working

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CustomerLandingView from "./views/CustomerLandingView"
+import ConnectedLogin from "./views/Login"
 
 const Index = () => <h2>HOME</h2>
 
@@ -13,12 +14,16 @@ const AppRouter = () => (
                         <Link to="/">Home</Link>
                     </li>
                     <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
                         <Link to="/customer/">Customer Page</Link>
                     </li>
                 </ul>
             </nav>
 
             <Route path="/" exact component={Index} />
+            <Route path="/login" exact component={ConnectedLogin} />
             <Route path="/customer/" component={CustomerLandingView} />
         </div>
     </Router>
